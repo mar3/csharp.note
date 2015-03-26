761cd085e5fda1f2f6232e1336a31ac70287faa5
mass10 <irisawa.masaru@gmail.com>
2015-03-26 23:59:50 +0900

    wip

--- code.example/oraxx/oraxx/DataService.cs
+++ code.example/oraxx/oraxx/DataService.cs
@@ -24,24 +24,63 @@ namespace oraxx
 			return this._connection;
 		}
 
-		public System.Data.IDataReader Query(string sql)
+		private static void Close(System.Data.IDataReader reader)
 		{
-			if (this._reader != null)
-				this._reader.Close();
+			if (reader == null)
+				return;
+			reader.Close();
+		}
+
+		private static void Close(System.Data.IDbConnection connection)
+		{
+			if (connection == null)
+				return;
+			connection.Close();
+		}
+
+		private void Free()
+		{
+			Close(this._reader);
+			this._reader = null;
+		}
+
+		private System.Data.IDbCommand CreateCommand(string sql)
+		{
 			System.Data.IDbConnection connection = this.Connect();
 			System.Data.IDbCommand command = connection.CreateCommand();
 			command.CommandText = sql;
+			return command;
+		}
+
+		public System.Data.IDataReader Query(string sql)
+		{
+			this.Free();
+			System.Data.IDbCommand command = this.CreateCommand(sql);
 			this._reader = command.ExecuteReader();
 			return this._reader;
 		}
 
+		public int Execute(string sql)
+		{
+			try
+			{
+				System.Data.IDbCommand command = this.CreateCommand(sql);
+				return command.ExecuteNonQuery();
+			}
+			catch (Oracle.ManagedDataAccess.Client.OracleException e)
+			{
+				if (e.Number == 1)
+					return 0; // UNIQUE CONSTRAINT VIOLATION
+				throw e;
+			}
+		}
+
 		public void Close()
 		{
-			if (this._reader != null)
-				this._reader.Close();
+			Close(this._reader);
 			this._reader = null;
-			if (this._connection != null)
-				this._connection.Close();
+	
+			Close(this._connection);
 			this._connection = null;
 		}
 	}
