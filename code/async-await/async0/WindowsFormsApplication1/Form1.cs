8b2a45458df86328c1cb45e2e077272fbc095bca
mass10 <irisawa.masaru@gmail.com>
2015-05-09 12:59:30 +0900

    wip

---
+++ code/async-await/async0/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Logger.Info("(ダイアログ) 非同期操作 呼び出し開始");
+
+			//重い処理の非同期呼び出し
+			LongBlockA();
+			
+			//終わるまで待つことも可能
+			//LongBlockA().GetAwaiter();
+			
+			Logger.Info("(ダイアログ) 非同期操作 呼び出し終了");
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("こんにちは。この操作はウィンドウをブロックしています。");
+		}
+
+		private static async Task LongBlockA()
+		{
+			await Task.Run(() =>
+			{
+				Logger.Info("(非同期操作) 重い処理の開始");
+				Thread.Sleep(1000 * 10);
+				Logger.Info("(非同期操作) 重い処理の終了");
+			});
+		}
+	}
+}
---
+++ code/async-await/async0/WindowsFormsApplication1/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+	internal static class Logger
+	{
+		private static string GetTimestamp()
+		{
+			DateTime now = DateTime.Now;
+			string timestamp = now.ToString("yyyy-M-dd HH:mm:ss") + String.Format(".{0:000}", now.Millisecond);
+			return timestamp;
+		}
+
+		public static void Info(params object[] parameters)
+		{
+			string current_timestamp = GetTimestamp();
+			string thread_id = String.Format("{0:X8}", Thread.CurrentThread.ManagedThreadId);
+
+			StringBuilder buffer = new StringBuilder();
+			buffer.Append(current_timestamp);
+			buffer.Append(" ");
+			buffer.Append("[INFO]");
+			buffer.Append(" ");
+			buffer.Append("<THREAD:");
+			buffer.Append(thread_id);
+			buffer.Append("> ");
+
+			foreach (var e in parameters)
+			{
+				buffer.Append(e);
+			}
+
+			Debug.WriteLine(buffer);
+		}
+	}
+}
