dca312bca5c6a0647e2b93414137d93b9cfb359b
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-04-29 13:35:13 +0900

    wip

--- code/network/amazon-s3/download/Program.cs
+++ code/network/amazon-s3/download/Program.cs
@@ -15,9 +15,19 @@ namespace download
 		{
 			try
 			{
+				Console.WriteLine("[TRACE] ### START ###");
+				Console.WriteLine("[TRACE] バケットを列挙しています...");
 				ListBucketsFromAmazonS3();
+				Console.WriteLine();
 
+				Console.WriteLine("[TRACE] バケット内のエントリーを列挙しています...");
 				EnumerateObject3("バケット名");
+				Console.WriteLine();
+
+				DownloadDirectory("バケット名", "パス");
+				Console.WriteLine();
+
+				Console.WriteLine("[TRACE] --- END ---");
 			}
 			catch (Exception e)
 			{
@@ -32,12 +42,11 @@ namespace download
 		/// </summary>
 		private static void ListBucketsFromAmazonS3()
 		{
-			var accessKeyId = "" + System.Configuration.ConfigurationManager.AppSettings["aws_access_key_id"];
-			var secretAccessKey = "" + System.Configuration.ConfigurationManager.AppSettings["aws_secret_access_key"];
+			var conf = new Configuration();
 
 			var s3 = new Amazon.S3.AmazonS3Client(
-				accessKeyId,
-				secretAccessKey,
+				conf.AccessKeyId,
+				conf.SecretAccessKey,
 				Amazon.RegionEndpoint.APNortheast1);
 
 			var buckets = s3.ListBucketsAsync();
@@ -54,7 +63,12 @@ namespace download
 		/// <param name="bucketName">バケット</param>
 		private static void EnumerateObject3(string bucketName)
 		{
-			var client = new Amazon.S3.AmazonS3Client(Amazon.RegionEndpoint.APNortheast1);
+			var conf = new Configuration();
+
+			var client = new Amazon.S3.AmazonS3Client(
+				conf.AccessKeyId,
+				conf.SecretAccessKey,
+				Amazon.RegionEndpoint.APNortheast1);
 
 			string key = "";
 
@@ -67,8 +81,6 @@ namespace download
 					// 次のページ
 					request.ContinuationToken = key;
 				}
-
-				Console.WriteLine("[TRACE] (ListObjects)");
 
 				using (var response = client.ListObjectsV2Async(request))
 				{
@@ -90,6 +102,77 @@ namespace download
 					}
 					Console.WriteLine("[TRACE] 次のページ: [" + key + "]");
 				}
+			}
+		}
+
+		private static void DownloadDirectory(string bucketName, string path)
+		{
+
+		}
+	}
+
+	internal sealed class Configuration
+	{
+		private string accessKeyId = "";
+
+		private string secretAccessKey = "";
+
+		/// <summary>
+		/// コンストラクター
+		/// </summary>
+		public Configuration()
+		{
+			// 既定のコンフィギュレーション
+			this.Configure();
+			// 環境変数によるコンフィギュレーションの書き換え
+			this.ConfigureFromEnv();
+		}
+
+		/// <summary>
+		/// コンフィギュレーション
+		/// </summary>
+		private void Configure()
+		{
+			this.accessKeyId = "" + System.Configuration.ConfigurationManager.AppSettings["aws_access_key_id"];
+			this.secretAccessKey = "" + System.Configuration.ConfigurationManager.AppSettings["aws_secret_access_key"];
+		}
+
+		/// <summary>
+		/// 環境変数によるコンフィギュレーションの書き換え
+		/// </summary>
+		private void ConfigureFromEnv()
+		{
+			// 環境変数による設定を検出したら、値が上書きされます。
+			this.accessKeyId = SelectValid(
+				Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID"), this.accessKeyId);
+			this.secretAccessKey = SelectValid(
+				Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY"), this.secretAccessKey);
+		}
+
+		/// <summary>
+		/// 有効な値の選択(left 優先)
+		/// </summary>
+		/// <param name="left"></param>
+		/// <param name="right"></param>
+		/// <returns></returns>
+		private static string SelectValid(string left, string right)
+		{
+			return string.IsNullOrEmpty(left) ? right : left;
+		}
+
+		public string AccessKeyId
+		{
+			get
+			{
+				return "" + this.accessKeyId;
+			}
+		}
+
+		public string SecretAccessKey
+		{
+			get
+			{
+				return "" + this.secretAccessKey;
 			}
 		}
 	}
