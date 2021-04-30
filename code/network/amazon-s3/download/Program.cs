601361f162619af0e0813f35a3540422e7ae544c
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-04-30 23:50:20 +0900

    wip

--- code/network/amazon-s3/download/Program.cs
+++ code/network/amazon-s3/download/Program.cs
@@ -54,7 +54,7 @@ namespace download
 		{
 			var conf = new Configuration();
 
-			var s3 = new Amazon.S3.AmazonS3Client(
+			using var s3 = new Amazon.S3.AmazonS3Client(
 				conf.AccessKeyId,
 				conf.SecretAccessKey,
 				Amazon.RegionEndpoint.APNortheast1);
@@ -73,31 +73,39 @@ namespace download
 		/// <param name="bucketName">バケット</param>
 		private static void EnumerateObject3(string bucketName)
 		{
-			var conf = new Configuration();
-
-			var s3 = new Amazon.S3.AmazonS3Client(
-				conf.AccessKeyId,
-				conf.SecretAccessKey,
-				Amazon.RegionEndpoint.APNortheast1);
-
-			string key = "";
-
-			while (true)
+			try
 			{
-				var request = new Amazon.S3.Model.ListObjectsV2Request { BucketName = bucketName, Prefix = "" };
+				Console.WriteLine("[TRACE] --- BUCKET: [" + bucketName + "] ---");
 
-				if (key != "")
-				{
-					// 次のページ
-					request.ContinuationToken = key;
-				}
+				uint objectCount = 0;
 
-				using (var response = s3.ListObjectsV2Async(request))
+				var conf = new Configuration();
+
+				// リージョン違う
+				var regionEndPoint = Amazon.RegionEndpoint.APNortheast1;
+
+				using var s3 = new Amazon.S3.AmazonS3Client(
+					conf.AccessKeyId,
+					conf.SecretAccessKey,
+					regionEndPoint);
+
+				string token = "";
+
+				while (true)
 				{
+					var request = new Amazon.S3.Model.ListObjectsV2Request { BucketName = bucketName, Prefix = "" };
+					if (token != "")
+					{
+						// 次のページ
+						request.ContinuationToken = token;
+					}
+
+					using var response = s3.ListObjectsV2Async(request);
 					var result = response.Result;
 					foreach (var e in result.S3Objects)
 					{
 						Console.WriteLine("[TRACE] S3 Object: " + e.Key + " (Truncated: " + result.IsTruncated + ")");
+						objectCount++;
 					}
 
 					// 次のページの有無を判断
@@ -105,13 +113,20 @@ namespace download
 					{
 						break;
 					}
-					key = "" + result.ContinuationToken;
-					if (key == "")
+					token = "" + result.ContinuationToken;
+					if (token == "")
 					{
 						break;
 					}
-					Console.WriteLine("[TRACE] 次のページ: [" + key + "]");
+
+					Console.WriteLine("[TRACE] この応答には次のページがあります。 token: [" + token + "]");
 				}
+
+				Console.WriteLine("[TRACE] " + objectCount + " 個のオブジェクトがみつかりました。");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("[ERROR] 予期しない例外です。" + e.Message);
 			}
 		}
 
@@ -119,39 +134,67 @@ namespace download
 		/// コンテンツをダウンロードします。
 		/// </summary>
 		/// <param name="bucketName">S3 バケットの名前</param>
-		/// <param name="path"></param>
+		/// <param name="key"></param>
 		/// <param name="localLocation"></param>
-		private static void DownloadDirectory(string bucketName, string path, string localLocation)
+		private static void DownloadDirectory(string bucketName, string key, string localLocation)
 		{
-			var conf = new Configuration();
-
-			var s3 = new Amazon.S3.AmazonS3Client(
-				conf.AccessKeyId,
-				conf.SecretAccessKey,
-				Amazon.RegionEndpoint.APNortheast1);
-
-			string key = "";
-
-			while (true)
+			try
 			{
-				var request = new Amazon.S3.Model.ListObjectsV2Request { BucketName = bucketName, Prefix = "" };
+				var conf = new Configuration();
 
-				if (key != "")
-				{
-					// 次のページ
-					request.ContinuationToken = key;
-				}
+				var s3 = new Amazon.S3.AmazonS3Client(
+					conf.AccessKeyId,
+					conf.SecretAccessKey,
+					Amazon.RegionEndpoint.APNortheast1);
 
-				using (var response = s3.ListObjectsV2Async(request))
+				string token = "";
+
+				while (true)
 				{
-					var result = response.Result;
+					var request = new Amazon.S3.Model.ListObjectsV2Request { BucketName = bucketName, Prefix = "" + key };
+
+					if (token != "")
+					{
+						// 次のページ
+						request.ContinuationToken = token;
+					}
+
+					using var listObjectsResponse = s3.ListObjectsV2Async(request);
+					var result = listObjectsResponse.Result;
 					foreach (var e in result.S3Objects)
 					{
+						// オブジェクト取り出し要求
 						var gor = new Amazon.S3.Model.GetObjectRequest();
-						gor.BucketName = "";
-						gor.Key = "";
-						var res = s3.GetObjectAsync(gor).Result;
-						Console.WriteLine("[TRACE] S3 Object: " + e.Key + " (Truncated: " + result.IsTruncated + ")");
+						gor.BucketName = bucketName;
+						gor.Key = e.Key;
+						using var objectResponse = s3.GetObjectAsync(gor);
+						using var res = objectResponse.Result;
+						var lastModified = res.LastModified;
+
+						if (!e.Key.StartsWith(key))
+							throw new Exception("Key が不正です。");
+
+						if (e.Key.EndsWith("/"))
+						{
+							// Must be application/x-directory
+							var relativeKey = e.Key.Substring(key.Length).Replace('/', System.IO.Path.DirectorySeparatorChar);
+							relativeKey = RemoveTailSlash(relativeKey);
+							var localPathName = System.IO.Path.Combine(localLocation, relativeKey);
+							System.IO.Directory.CreateDirectory(localPathName);
+							Console.WriteLine("[TRACE] create directory... [" + e.BucketName + "/" + e.Key + "] (" + res.Headers.ContentType + ")\n     >> [" + localPathName + "] (Truncated: " + result.IsTruncated + ")");
+							Console.WriteLine();
+						}
+						else
+						{
+							// Something else
+							var relativeKey = e.Key.Substring(key.Length).Replace('/', System.IO.Path.DirectorySeparatorChar);
+							var localPathName = System.IO.Path.Combine(localLocation, relativeKey);
+							CreateParentDirectory(localPathName);
+							System.Threading.CancellationToken cancellationToken;
+							res.WriteResponseStreamToFileAsync(localPathName, false, cancellationToken).Wait();
+							Console.WriteLine("[TRACE] DOWNLOADING FILE... [" + e.BucketName + "/" + e.Key + "] (" + res.Headers.ContentType + ")\n     >> [" + localPathName + "] (Truncated: " + result.IsTruncated + ")");
+							Console.WriteLine();
+						}
 					}
 
 					// 次のページの有無を判断
@@ -159,14 +202,34 @@ namespace download
 					{
 						break;
 					}
-					key = "" + result.ContinuationToken;
-					if (key == "")
+					token = "" + result.ContinuationToken;
+					if (token == "")
 					{
 						break;
 					}
-					Console.WriteLine("[TRACE] 次のページ: [" + key + "]");
+					Console.WriteLine("[TRACE] 次のページ: [" + token + "]");
+
 				}
 			}
+			catch (Exception e)
+			{
+				Console.WriteLine("[ERROR] 予期しない例外です。" + e.Message);
+				Console.WriteLine();
+			}
+		}
+
+		private static void CreateParentDirectory(string path)
+		{
+			var parent = System.IO.Path.GetDirectoryName(path);
+			if (System.IO.Directory.Exists(parent))
+				return;
+			System.IO.Directory.CreateDirectory(parent);
+		}
+
+		private static string RemoveTailSlash(string key)
+		{
+			if (key == "") return key;
+			return key.Substring(0, key.Length - 1);
 		}
 	}
 
