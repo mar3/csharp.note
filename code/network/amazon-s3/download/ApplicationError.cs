e62dc93601222c88eede5e950644b8dc2759fe38
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-05-01 11:47:38 +0900

    wip

---
+++ code/network/amazon-s3/download/ApplicationError.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace download
+{
+    internal sealed class ApplicationError : Exception
+    {
+        private string _description = "";
+
+        public ApplicationError(string description)
+		{
+            _description = description;
+        }
+
+        public override string ToString()
+		{
+            return this._description;
+		}
+
+		public override string Message
+		{
+            get
+			{
+				return this._description;
+			}
+		}
+	}
+}
